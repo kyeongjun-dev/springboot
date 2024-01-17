a5e2ee42271f118cd276cb68e5678464e318a47c
kyeongjun-dev <kyeongjun.dev@gmail.com>
2024-01-18 00:08:23 +0900

    url one, all 별로 다른 쿼리를 수행하도록 변경

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
-import org.springframework.data.mongodb.core.aggregation.AggregationOptions;
 
 import java.util.List;
 
@@ -22,23 +19,33 @@ public class DemoApplication {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-	@RequestMapping("/")
+	@RequestMapping("/one")
 	String home() {
-		// // // // // //
 		// find 쿼리
 		Query query = new Query();
 		query.addCriteria(Criteria.where("name").is("kkj"));
-		// query.addCriteria(Criteria.where("keywords.keyword").is("t").and("cdIdx").is(2));
 		// query.cursorBatchSize(100);
-		// List<String> documents = mongoTemplate.findAll(String.class, "employees");
 		List<String> documents = mongoTemplate.find(query, String.class, "employees");
-		// // // // // //
 
         // 조회된 문서 출력
         StringBuilder result = new StringBuilder("MongoDB Documents:\n");
         documents.forEach(document -> result.append(document).append("\n"));
 
-        return result.toString();
+        return "Number of MongoDB Documents: " + documents.size();
+	}
+
+	@RequestMapping("/all")
+	String all() {
+		// find all 쿼리
+		Query query = new Query();
+		query.cursorBatchSize(100);
+		List<String> documents = mongoTemplate.find(query, String.class, "employees");
+
+        // 조회된 문서 출력
+        // StringBuilder result = new StringBuilder("MongoDB Documents:\n");
+        // documents.forEach(document -> result.append(document).append("\n"));
+
+        return "Number of MongoDB Documents: " + documents.size();
 	}
 
 	public static void main(String[] args) {
